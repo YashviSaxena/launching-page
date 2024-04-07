635861cb773e2122b8f92aea966ebc45590fdb8f
YashviSaxena <yashvi4ys@gmail.com>
2024-04-07 14:37:19 +0530

    changes

--- src/components/AnimatedTextWord.jsx
+++ src/components/AnimatedTextWord.jsx
@@ -1,76 +1,13 @@
-import React, { useEffect, useRef } from "react";
-import { motion } from "framer-motion";
+import React from 'react'
 
-const AnimatedTextWord = ({ text }) => {
-  const words = text.split(" ");
-  const containerRef = useRef(null);
-
-  // Variants for Container of words.
-  const container = {
-    hidden: { opacity: 0 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
-    }),
-  };
-
-  // Variants for each word.
-  const child = {
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        type: "spring",
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-    hidden: {
-      opacity: 0,
-      x: 20,
-      transition: {
-        type: "spring",
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-  };
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      containerRef.current && containerRef.current.setProps({ animate: "visible" });
-    }, 500); // Adjust the delay to match your animation duration
-
-    return () => clearTimeout(timeout);
-  }, []);
-
-  return (
-    <motion.div
-      ref={containerRef}
-      style={{
-        overflow: "hidden",
-        textAlign: "center",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "2rem",
-      }}
-      variants={container}
-      initial="hidden"
-      animate="visible"
-    >
-      {words.map((word, index) => (
-        <motion.span
-          variants={child}
-          style={{ marginRight: "5px" }}
-          key={index}
-        >
-          {word}
-        </motion.span>
-      ))}
-    </motion.div>
-  );
-};
+const AnimatedTextWord = () => {
+  return(
+    <div>
+       <div className='text-wrapper'>
+          <h1 className="text-cyan-300 font-semibold text-3xl text-center mt-8">Coming&nbsp;&nbsp;Soon</h1>
+       </div>
+    </div>
+  )
+}
 
 export default AnimatedTextWord;
