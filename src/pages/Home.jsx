5602ea6c8286a67b0ca27817c6537cb57594d757
YashviSaxena <yashvi4ys@gmail.com>
2024-04-08 17:57:10 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,9 @@ import straight from "../assets/straight.svg";
 import Phone from "../components/Phone";
 import vector7575 from "../assets/Group 7575.svg";
 
-
-
 const Home = () => {
   return (
-    <div className="h-screen w-screen overflow-hidden bg-[#03022A] px-48">
+    <div className="lg:h-screen  w-screen bg-[#03022A] xl:px-48 lg:px-32 px-20">
       <img
         src={straight}
         alt=""
@@ -18,55 +16,64 @@ const Home = () => {
       <img
         src={vector7575}
         alt=""
-        className="absolute lg:block hidden top-32 w-[700px] ml-16    -z-1"
+        className="absolute lg:block hidden top-32 w-[700px] ml-16 -z-1"
       />
+
       <div className="flex justify-start items-center">
-        <img src={logo} alt="" className="mt-10 h-12 " />
-        <img src={ns} alt="" className="mt-12 h-8 ml-2" />
+        <img src={logo} alt="" className="mt-10 md:h-12 h-8 " />
+        <img src={ns} alt="" className="mt-12 md:h-8 ml-2 h-5" />
       </div>
 
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col z-20">
-          <div className="mt-20 flex py-2 rounded-full w-[400px] items-center h-8  border border-cyan-500/50 backdrop-blur-lg bg-[#03022A] lg:mr-28 ">
-            <p className="text-white text-center text-sm font-semibold border border-cyan-500/50 mr-4 py-[5px] text-[10px] bg-[#070952a4] px-5  rounded-full">
+ 
+        {/*main-code below */}
+
+
+      <div className="flex lg:flex-row flex-col justify-center lg:justify-between items-center">
+        <div className="flex justify-center items-center flex-col z-20">
+
+          <div className="mt-20 flex py-2 rounded-full sm:w-[400px] w-[350px] items-center sm:h-8 h-7  border border-cyan-500/50 backdrop-blur-lg bg-[#03022A] ">
+            <p className="text-white text-center text-xs sm:text-sm font-semibold border border-cyan-500/50 mr-4 py-[5px] text-[10px] bg-[#070952a4] px-5  rounded-full">
               New
             </p>
-            <p className="text-gray-100 text-sm font-semibold">
+            <p className="text-gray-100 sm:text-sm text-xs font-semibold">
               Neo Saturn Mobile Application is coming soon
             </p>
           </div>
-          <div className="mt-8">
-            <h1 className="text-white text-3xl font-semibold w-[425px]">
+          <div className="flex flex-col justify-center items-center mt-8">
+            <h1 className="text-white flex justify-center items-center sm:text-3xl text-2xl font-semibold w-[425px]">
               Disrupting the way Banking
               <br />
               works, one segment at a time.
             </h1>
-            <p className="mt-5 text-white text-sm w-[400px]">
+            <p className="mt-5 text-white text-sm sm:w-[400px] w-[340px]">
               Redefining banking: Empowering everyone, everywhere with
               innovative financial solutions.
             </p>
-            <div className="flex flex-col  justify-start">
+            <div className="flex flex-col justify-start">
               <input
                 type="tel"
                 placeholder="Enter phone number"
                 name="phone"
-                className="mt-5 z-40 bg-blue-400/20 px-5 focus:ring-1 focus:ring-blue-500 border-2 border-blue-500/10 backdrop-blur-lg outline-none text-sm text-white rounded-md h-10 w-[400px]"
+                className="mt-5 z-40 bg-blue-400/20 px-5 focus:ring-1 focus:ring-blue-500 border-2 border-blue-500/10 backdrop-blur-lg outline-none text-sm text-white rounded-md h-10 sm:w-[400px] w-[350px]"
               />
-<button class="mt-5 hover:shadow-4xl transition-all ease-in-out duration-300 shadow-3xl hover:ring-3 z-4 bg-[#323BFF] hover:bg-[#326BFF]  font-semibold   w-[400px] rounded-tl-2xl  rounded-br-2xl h-10 text-white relative overflow-hidden focus:ring-2 outline-none focus:ring-[#468abd]">
+<button class="mt-5 hover:shadow-4xl transition-all ease-in-out duration-300 shadow-3xl hover:ring-3 z-4 bg-[#323BFF] hover:bg-[#326BFF]  font-semibold   sm:w-[400px] w-[350px] rounded-tl-2xl  rounded-br-2xl h-10 text-white relative overflow-hidden focus:ring-2 outline-none focus:ring-[#468abd]">
     Notify me
 </button>
 
             </div>
           </div>
-        </div>
-
-        <Phone />
-        <img
+          <img
           src={straight}
           alt=""
 
-          className="absolute md:right-24 right-12 h-[500px] mt-10"
+          className="absolute md:right-24 sm:right-12 right-8 h-[500px] bottom-10"
         />
+        </div>
+        
+        <div className="lg:mt-0 mt-20">
+        <Phone />
+        </div>
+      
          {/* <div className="radial-blurs lg:w-[170px] lg:h-[170px] bg-[#0c65ff] absolute top-[5%] left-[0%] blur-[100px] -z-1"></div>
        
       <div className="radial-blurs lg:w-[150px] lg:h-[150px] bg-[#0CF0FF] absolute top-[48%] left-[23%] blur-[130px] -z-1"></div>
